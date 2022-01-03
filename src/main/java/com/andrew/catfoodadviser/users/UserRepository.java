7089e649ce236f7a9398c5a3191af821b070ea3b
ydm2790 <ydm2790@gmail.com>
2022-01-03 11:28:02 +0900

    configuring :: JDBC -> JPA

--- src/main/java/com/andrew/catfoodadviser/users/UserRepository.java
+++ src/main/java/com/andrew/catfoodadviser/users/UserRepository.java
@@ -1,10 +1,9 @@
 package com.andrew.catfoodadviser.users;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     void update(User user);
-    Optional<User> findById(long id);
     Optional<User> findByEmail(Email email);
 }
